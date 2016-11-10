b33071d4993047ffc2eb6539b1a92eaf2f8c5555
Bob Deckard <bdeckard92@gmail.com>
2016-11-10 14:30:30 -0600

    Nothing works!

---
+++ index.html/assets/javascript/game.js
@@ -0,0 +1,40 @@
+
+
+console.log(5+6)
+
+
+	//Zombie Hangman- 
+
+
+	var word = [“brains”, “undead”, “walker”, “corpse”, “creepy”, “terror”, “wicked”, “zombie”, “scared”, “scream”, “killer”, “putrid”];
+	var guess = prompt(“Enter a letter before the Zombies get you!”);
+//Choose word at random and present to function as string for checking
+	var wordChoose = word[Math.floor(Math.random() * word.lentght)]; 
+	console.log(wordChoose);
+
+for (i = 0; i < wordChoose.length; i++)
+		//* word.indexOf  while loop while(index >=o) for(var i= 0; i < word.length;
+var index=word.indexOf(guess);
+dashes[index] = guess;
+while(index >=0){
+	Index= word.indexOf(guess, index + 1)
+	If (index !== -1){
+		dashes [index] = guess
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
