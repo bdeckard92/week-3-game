20c36fb60128be409ab3fee9d1e4ae8c1963f2e7
Bob Deckard <bdeckard92@gmail.com>
2016-11-10 18:01:36 -0600

    wow

--- index.html/assets/javascript/game.js
+++ index.html/assets/javascript/game.js
@@ -1,24 +1,24 @@
 
 	var word = ["brains", "undead", "walker", "corpse", "creepy", "terror", "wicked", "zombie", "scared", "scream", "killer", "putrid"]
+	var num_lives = 0
 	
-//Choose word at random and present to function as string for checking
 	var wordChoose = word[Math.floor(Math.random() * word.length)]; 
 	console.log(wordChoose);
 
-document.onkeyup =function(event)
+//document.onkeyup =function(event){
 
-for (i = 0; i < wordChoose.length; i++){
+// for (i = 0; i < wordChoose.length; i++){
 		//* word.indexOf  while loop while(index >=o) for(var i= 0; i < word.length;
 	//var index=word.indexOf(guess);
 	//dashes[index] = guess;
 
-	while(index >=0){
-		index= word.indexOf(guess, index + 1)
-		if (index !== -1){
-			dashes [index] = guess
-		}
-	}
-}
+	// while(index >=0){
+	// 	index= word.indexOf(guess, index + 1)
+	// 	if (index !== -1){
+	// 		dashes [index] = guess
+		
+	
+
 
 	//Zombie Hangman- 
 
